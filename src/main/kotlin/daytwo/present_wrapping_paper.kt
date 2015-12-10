247e367bd69750e599c5dd1636c1f0725163bcbf
Filipe Guedes <fgs.guedes@gmail.com>
2015-12-10 00:34:58 -0200

    Day 2 - main method to run my puzzle input

--- src/main/kotlin/daytwo/present_wrapping_paper.kt
+++ src/main/kotlin/daytwo/present_wrapping_paper.kt
@@ -1,5 +1,8 @@
 package daytwo
 
+import java.nio.file.Files
+import java.nio.file.Paths
+
 fun requiredPaper(input: String): Int {
   val (length, height, width) = input.split("x").map { it.toInt() }
 
@@ -21,4 +24,17 @@ data class Box(val front: Face, val top: Face, val side: Face) {
 
 data class Face(val width: Int, val height: Int) {
   fun area() = width * height
+}
+
+fun main(args: Array<String>) {
+  execute("pretent_wrapping_paper_input.txt") { fileContent ->
+    fileContent.map { requiredPaper(it) }.sum()
+  }
+}
+
+fun execute(inputeFileName: String, f: (List<String>) -> Any) {
+  val path = Paths.get("src", "main", "resources", "daytwo", inputeFileName)
+  val input = Files.readAllLines(path)
+  val resultingFloor = f(input)
+  println(resultingFloor)
 }
