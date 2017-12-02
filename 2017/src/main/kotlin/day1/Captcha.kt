0ce432c33ab491e517b0629c620d48a1602122cf
Filipe Guedes <203216+fgsguedes@users.noreply.github.com>
2017-12-02 00:59:35 -0200

    2017, Day 1 - Captcha part 2 and improved part 1 solution

--- 2017/src/main/kotlin/day1/Captcha.kt
+++ 2017/src/main/kotlin/day1/Captcha.kt
@@ -3,15 +3,28 @@ package day1
 import fromOneLineInput
 import java.lang.Character.getNumericValue
 
-fun solveCaptcha(input: String) = input.foldIndexed(0) { index, sum, char ->
-  if (index == input.length - 1) {
-    if (input[0] == char) getNumericValue(char) + sum
-    else sum
-  } else if (char == input[index + 1]) getNumericValue(char) + sum
+fun solveCaptchaPart1(input: String) = input.foldIndexed(0) { index, sum, char ->
+  val nextChar = input[(index + 1) % input.length]
+
+  if (char == nextChar) getNumericValue(char) + sum
   else sum
 }
 
+fun solveCaptchaPart2(input: String): Int {
+  val step = input.length / 2
+
+  return input.foldIndexed(0) { index, sum, char ->
+    val nextChar = input[(index + step) % input.length]
+
+    if (char == nextChar) getNumericValue(char) + sum
+    else sum
+  }
+}
+
 fun main(args: Array<String>) {
-  val solution = fromOneLineInput(2017, 1, "Captcha.txt", ::solveCaptcha)
-  println(solution)
+  val part1Solution = fromOneLineInput(2017, 1, "Captcha.txt", ::solveCaptchaPart1)
+  println(part1Solution)
+
+  val part2Solution = fromOneLineInput(2017, 1, "CaptchaPart2.txt", ::solveCaptchaPart2)
+  println(part2Solution)
 }
--- 2017/src/test/kotlin/day1/CaptchaTest.kt
+++ 2017/src/test/kotlin/day1/CaptchaTest.kt
@@ -1,15 +1,30 @@
 package day1
 
 import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertAll
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.function.Executable
 
 class CaptchaTest {
 
   @Test
   fun testPart1ExampleInput() {
-    assertEquals(3, solveCaptcha("1122"))
-    assertEquals(4, solveCaptcha("1111"))
-    assertEquals(0, solveCaptcha("1234"))
-    assertEquals(9, solveCaptcha("91212129"))
+    assertAll(
+        Executable { assertEquals(3, solveCaptchaPart1("1122")) },
+        Executable { assertEquals(4, solveCaptchaPart1("1111")) },
+        Executable { assertEquals(0, solveCaptchaPart1("1234")) },
+        Executable { assertEquals(9, solveCaptchaPart1("91212129")) }
+    )
+  }
+
+  @Test
+  fun testPart2ExampleInput() {
+    assertAll(
+        Executable { assertEquals(6, solveCaptchaPart2("1212")) },
+        Executable { assertEquals(0, solveCaptchaPart2("1221")) },
+        Executable { assertEquals(4, solveCaptchaPart2("123425")) },
+        Executable { assertEquals(12, solveCaptchaPart2("123123")) },
+        Executable { assertEquals(4, solveCaptchaPart2("12131415")) }
+    )
   }
 }
